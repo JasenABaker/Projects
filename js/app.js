a764fb10b68f16f26d36be48124a5ae56294632a
JasenABaker <JasenABaker@gmail.com>
2017-12-13 12:26:18 -0500

    got answer check funtion working

--- js/app.js
+++ js/app.js
@@ -40,9 +40,9 @@ $(document).ready(function () {
     const catFourQuestion = []
     const catFiveQuestion = []
     ///fake answer array
-    const catOneFake = ['Who is Bob Hoskins', 'Who is Phill Murray','Who is Gabriel Hawkins', 'Who is Nick Coppala','Who is Ben Weinman', 'Who is Greg Puciato', 'Who is Liam Wilson',
-    'Who is John Dolmayan', 'Who is Daron Malakian', 'Who is Andy Khachaturian','Who is Juan Alderete', 'Who is Cedric Bixler-Zavala', 'Who is Deantoni Parks' ]
-    const catTwoFake =[]
+    const catOneFake = ['Who is Bob Hoskins', 'Who is Phill Murray', 'Who is Gabriel Hawkins', 'Who is Nick Coppala', 'Who is Ben Weinman', 'Who is Greg Puciato', 'Who is Liam Wilson',
+        'Who is John Dolmayan', 'Who is Daron Malakian', 'Who is Andy Khachaturian', 'Who is Juan Alderete', 'Who is Cedric Bixler-Zavala', 'Who is Deantoni Parks']
+    const catTwoFake = []
     const catThreeFake = []
     const catFourFake = []
     const catFiveFake = []
@@ -53,14 +53,27 @@ $(document).ready(function () {
 
 
     const game = {
-            // get answer for category and point value
+        catHolder: 0,
+        pointhHolder: 0,
+        // get answer for category and point value
         answer: (category, pointValue) => {
 
             let selectAnswerCat = answers[category]
-            let selcetCatPoint = selectAnswerCat[pointValue];
+            let selectCatPoint = selectAnswerCat[pointValue];
             // console.log(category)
             // console.log(pointValue)
-            $(".answerCard").html(selcetCatPoint)
+            
+           $(".answerCard").html(selectCatPoint)
+           game.catHolder = category
+           game.pointHolder = pointValue
+          // console.log(game.catHolder)
+          //console.log(game.pointHolder)
+        },
+        //get the correct question
+        correctQuestion: (category, pointValue) => {
+            const selectQuestionCat = correctQuestions[category]
+            const selectQuestion = selectQuestionCat[pointValue]  
+            return selectQuestion
         },
         // shuffle function found on internet devised by fisher-yates and Don knuth
         shuffle: (ray) => {
@@ -72,22 +85,35 @@ $(document).ready(function () {
 
         },
         // getting the correct question  and pushing to an array with shuffled wrong questions
-        question: (category, pointValue) => {
-            const selectQuestionCat = correctQuestions[category]
-            const selectQuestionPoint = selectQuestionCat[pointValue]
+        questionRandomizer: (category, pointValue) => {
+            const addRealQuestion = game.correctQuestion(category, pointValue)
             const selectFakeQestionCat = fakeAnswerArray[category]
             const fakeQestionCat = game.shuffle(selectFakeQestionCat)
             const newFakeArray = fakeQestionCat.slice(0, 3)
-            
-            newFakeArray.push(selectQuestionPoint)
-            const shuffleQuestion = game.shuffle(newFakeArray)
-            console.log(shuffleQuestion)
-             $("#qc1").html(shuffleQuestion[0])
-             $("#qc2").html(shuffleQuestion[1])
-            $("#qc3").html(shuffleQuestion[2])
-              $("#qc4").html(shuffleQuestion[3])
 
-    },
+            newFakeArray.push(addRealQuestion)
+            const shuffleQuestion = game.shuffle(newFakeArray)
+            // console.log(shuffleQuestion)
+            $("#qc1").html(shuffleQuestion[0])
+            $("#qc2").html(shuffleQuestion[1])
+            $("#qc3").html(shuffleQuestion[2])
+            $("#qc4").html(shuffleQuestion[3])
+
+        },
+
+        compareQuestion: (cat, point, spanClicked) => {
+           const questionSelected = $(`[data-span="${spanClicked}"]`).text()
+           const correct = game.correctQuestion(cat, point)
+            
+           if (questionSelected === correct){
+               console.log('You are correct!')
+           } else {
+               console.log('You are wrong!')
+           }
+
+           // console.log(questionSelected)
+            console.log(correct)
+        },
     }
 
 
@@ -115,10 +141,15 @@ $(document).ready(function () {
         let point = ($(this).data('point') - 1)
         // console.log(point)
         game.answer(cat, point)
-        game.question(cat, point)
+        game.correctQuestion(cat, point)
+        game.questionRandomizer(cat, point)
 
     })
 
-    questSpan.on('click', closeModal)
-
+    questSpan.on('click', function () {
+        let span = $(this).data('span')
+       // console.log(span)
+        game.compareQuestion(game.catHolder, game.pointHolder, span)
+        closeModal()
+    })
 })
