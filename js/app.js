b3acfacac88b1cb1181e7b78e99c61433902ec60
JasenABaker <JasenABaker@gmail.com>
2017-12-13 10:04:10 -0500

    added shuffling of fake questions with correct question

--- js/app.js
+++ js/app.js
@@ -22,21 +22,12 @@ $(document).ready(function () {
     }
 
 
-   
-    const fakeAnswers = {
-        pointOne: ['Who is Travis Stever', 'Who is Bob Hoskins', 'Who is Phill Murray'],
-        pointTwo: ['Who is Mike Todd', 'Who is Gabriel Hawkins', 'Who is Nick Coppala'],
-        pointThree: ['Who is Ben Weinman', 'Who is Greg Puciato', 'Who is Liam Wilson'],
-        pointFour: ['Who is John Dolmayan ', 'Who is Daron Malakian', 'Who is Andy Khachaturian'],
-        pointFive: ['Who is Juan Alderete', 'Who is Cedric Bixler-Zavala', 'Who is Deantoni Parks'],
-
-    }
 
     //arrays of answers
     const catOneAnswer = ['He is the lead singer',
         'He is the lead guitarist',
         'He is the drummer',
-        "He is the newest memeber of the band and he's also pretty good at base",
+        "He is the newest memeber of the band and he's also pretty good at bass",
         'An original member, he was replaced after robbing a CVS']
     const catTwoAnswer = []
     const catThreeAnswer = []
@@ -48,13 +39,21 @@ $(document).ready(function () {
     const catThreeQuestion = []
     const catFourQuestion = []
     const catFiveQuestion = []
-
+    ///fake answer array
+    const catOneFake = ['Who is Bob Hoskins', 'Who is Phill Murray','Who is Gabriel Hawkins', 'Who is Nick Coppala','Who is Ben Weinman', 'Who is Greg Puciato', 'Who is Liam Wilson',
+    'Who is John Dolmayan', 'Who is Daron Malakian', 'Who is Andy Khachaturian','Who is Juan Alderete', 'Who is Cedric Bixler-Zavala', 'Who is Deantoni Parks' ]
+    const catTwoFake =[]
+    const catThreeFake = []
+    const catFourFake = []
+    const catFiveFake = []
     ///arrays for the answer and question
     const answers = [catOneAnswer, catTwoAnswer, catThreeAnswer, catFourAnswer, catFiveAnswer]
     const correctQuestions = [catOneQuestion, catTwoQuestion, catThreeQuestion, catFourQuestion, catFiveQuestion]
+    const fakeAnswerArray = [catOneFake, catTwoFake, catThreeFake, catFourFake, catFiveFake]
 
 
     const game = {
+            // get answer for category and point value
         answer: (category, pointValue) => {
 
             let selectAnswerCat = answers[category]
@@ -63,6 +62,7 @@ $(document).ready(function () {
             // console.log(pointValue)
             $(".answerCard").html(selcetCatPoint)
         },
+        // shuffle function found on internet devised by fisher-yates and Don knuth
         shuffle: (ray) => {
             for (let i = ray.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
@@ -71,16 +71,21 @@ $(document).ready(function () {
             return ray;
 
         },
+        // getting the correct question  and pushing to an array with shuffled wrong questions
         question: (category, pointValue) => {
             const selectQuestionCat = correctQuestions[category]
             const selectQuestionPoint = selectQuestionCat[pointValue]
-            fakeAnswers.pointOne.push(selectQuestionPoint)
-            const shuffleQuestion = game.shuffle(fakeAnswers.pointOne)
+            const selectFakeQestionCat = fakeAnswerArray[category]
+            const fakeQestionCat = game.shuffle(selectFakeQestionCat)
+            const newFakeArray = fakeQestionCat.slice(0, 3)
+            
+            newFakeArray.push(selectQuestionPoint)
+            const shuffleQuestion = game.shuffle(newFakeArray)
             console.log(shuffleQuestion)
-               $("#qc1").html(shuffleQuestion[0])
-               $("#qc2").html(shuffleQuestion[1])
-               $("#qc3").html(shuffleQuestion[2])
-               $("#qc4").html(shuffleQuestion[3])
+             $("#qc1").html(shuffleQuestion[0])
+             $("#qc2").html(shuffleQuestion[1])
+            $("#qc3").html(shuffleQuestion[2])
+              $("#qc4").html(shuffleQuestion[3])
 
     },
     }
